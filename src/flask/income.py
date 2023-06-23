b95c7b89b3a970303ec3bbf329bd159c0806cf2d
Geoff Parris <geoffhparris@gmail.com>
2023-06-23 15:56:41 -0400

    income implemented

---
+++ src/flask/income.py
@@ -0,0 +1,14 @@
+def calc_income():
+	gross_wages = 100000
+	taxable_interest = 15000
+	dividends = 5000
+	qualified_dividends = 2500
+	ira_deduction = 10000
+	student_loan_interest = 4000
+	
+	income = (gross_wages +
+		taxable_interest +
+		(dividends - qualified_dividends) -
+		ira_deduction -
+		student_loan_interest)
+	return income
