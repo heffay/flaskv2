4c1af6bbb5e365a8a6d02dfea21548565272b4d0
Geoff Parris <geoffhparris@gmail.com>
2023-06-24 11:32:49 -0400

    another shot

--- src/flask/income.py
+++ src/flask/income.py
@@ -3,12 +3,12 @@ def calc_interest(amount, rate):
 	else: return 0
 
 def calc_income():
-	gross_wages = 100000
-	taxable_interest = 15000
-	dividends = 5000
-	qualified_dividends = 2500
-	ira_deduction = 10000
-	student_loan_interest = 4000
+	gross_wages 			= 100000
+	taxable_interest 		= 15000
+	dividends 				= 5000
+	qualified_dividends 	= 2500
+	ira_deduction 			= 10000
+	student_loan_interest 	= 4000
 	
 	income = ( gross_wages +
 		taxable_interest +
