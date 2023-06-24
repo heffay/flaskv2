180b9fc67b7d92398500990d9c6d69600bb971e4
Geoff Parris <geoffhparris@gmail.com>
2023-06-24 11:29:18 -0400

    better prompt

--- src/flask/income.py
+++ src/flask/income.py
@@ -1,3 +1,7 @@
+def calc_interest(amount, rate):
+	if( rate != 0 ): return amount*rate
+	else: return 0
+
 def calc_income():
 	gross_wages = 100000
 	taxable_interest = 15000
@@ -6,9 +10,9 @@ def calc_income():
 	ira_deduction = 10000
 	student_loan_interest = 4000
 	
-	income = (gross_wages +
+	income = ( gross_wages +
 		taxable_interest +
-		(dividends - qualified_dividends) -
+		( dividends - qualified_dividends ) -
 		ira_deduction -
-		student_loan_interest)
+		student_loan_interest )
 	return income
